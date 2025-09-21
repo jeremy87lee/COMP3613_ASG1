39a55fb68170cc6bccf8988e0b27d3b99fbd26bd
jeremy87lee <157717264+jeremy87lee@users.noreply.github.com>
2025-09-21 15:45:16 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -24,3 +24,29 @@ class User(db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+class Resident(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    license_number = db.Column(db.String(20), nullable=False)
+    vehicle_info = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(50), nullable=False)
+    
+class Drive(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    driver_id = db.Column(db.Integer, db.ForeignKey('driver.id'), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    
+class Stop(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    drive_id = db.Column(db.Integer, db.ForeignKey('drive.id'), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    resident_id = db.Column(db.Integer, db.ForeignKey('resident.id'), nullable=False)
+
