d533a3052f8c4c54adc58f524f2a0e6d86ec5706
jeremy87lee <157717264+jeremy87lee@users.noreply.github.com>
2025-09-23 22:50:59 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from flask.cli import with_appcontext, AppGroup
 from App.database import db, get_migrate
 from App.models import User, Resident, Driver, Drive, Stop
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize, create_drive, create_stop)
+from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize, create_drive, create_stop, viewInbox)
 
 
 # This commands file allow you to create convenient CLI commands for testing controllers
@@ -28,6 +28,8 @@ User Commands
 # create a group, it would be the first argument of the comand
 # eg : flask user <command>
 user_cli = AppGroup('user', help='User object commands') 
+driver_cli = AppGroup('driver', help='Driver object commands')
+resident_cli = AppGroup('resident', help='Resident object commands')
 
 # Then define the command and any parameters and annotate it with the group (@)
 @user_cli.command("create", help="Creates a user")
@@ -47,22 +49,28 @@ def list_user_command(format):
     else:
         print(get_all_users_json())
 
-@user_cli.command("schedule", help="Schedule a Drive to a Street")
+@driver_cli.command("schedule", help="Schedule a Drive to a Street")
 @click.argument("driver_id", type=int, default=1)
-@click.argument("street", type=str, default="1st Street")
+@click.argument("street", type=str, default="123 Main St")
 def schedule_drive_command(driver_id, street):
     create_drive(driver_id, street)
-    print(f'Drive scheduled for driver number {driver_id} on {street}!')
+    
 
-@user_cli.command("addstop", help="Add a Stop to a Drive")
+@resident_cli.command("addstop", help="Add a Stop to a Drive")
 @click.argument("drive_id", type=int, default=1)
-@click.argument("address", type=str, default="123 Main St")
+@click.argument("house_number", type=int, default=1)
 @click.argument("resident_id", type=int, default=1)
-def add_stop_command(drive_id, address, resident_id):
-    create_stop(drive_id, address, resident_id)
-    print(f'Stop added to drive number {drive_id} for resident number {resident_id} at {address}!')
+def add_stop_command(drive_id, house_number, resident_id):
+    create_stop(drive_id, house_number, resident_id)
+    drive = Drive.query.get(drive_id)
+    if drive: 
+        address = drive.street
+    else:
+        print(f'No drive found with ID {drive_id}')
+        return
+    print(f'Stop added to drive number {drive_id} for resident number {resident_id} at house number {house_number} on {address}!')
 
-@user_cli.command("viewdriver", help="View status and location of a Driver")
+@resident_cli.command("viewdriver", help="View status and location of a Driver")
 @click.argument("driver_id", type=int, default=1)
 def view_driver_command(driver_id):
     driver = db.session.get(Driver, driver_id)
@@ -72,18 +80,20 @@ def view_driver_command(driver_id):
     else:
         print(f'No driver found with ID {driver_id}')
 
-@user_cli.command("viewinbox", help="View Drives scheduled for a Street")
-@click.argument("street", type=str, default="1st Street")
-def view_inbox_command(street):
-    drives = db.session.query(Drive).filter(Drive.street == street).all()
-    if drives:
-        for drive in drives:
-            driver = db.session.get(Driver, drive.driver_id)
-            print(f'Drive Number: {drive.id}, Driver Name: {driver.name}, Street: {drive.street}')
-    else:
-        print(f'No drives scheduled for {street}')
+@resident_cli.command("viewinbox", help="View Drives scheduled for a Resident's street")
+@click.argument("resident_id", type=int, default=1)
+def view_inbox_command(resident_id):
+    resident = Resident.query.get(resident_id)
+    if not resident:
+        print(f'No resident found with ID {resident_id}')
+        return
+    street = resident.address
+    viewInbox(street)
+    
 
 app.cli.add_command(user_cli) # add the group to the cli
+app.cli.add_command(driver_cli)
+app.cli.add_command(resident_cli)
 
 '''
 Test Commands
@@ -102,5 +112,4 @@ def user_tests_command(type):
         sys.exit(pytest.main(["-k", "App"]))
     
 
-app.cli.add_command(test)
 app.cli.add_command(test)
